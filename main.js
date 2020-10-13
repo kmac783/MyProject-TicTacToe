393b1f1d4c122aedb30dde574836e17a602cb5d7
Kimberly Diehl <kimberly783@gmail.com>
2020-10-13 13:55:56 -0400

    including readme and final code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-console.log('im working');
+//console.log('im working');
 
 //There will be two players to start the game.
 let currentPlayer = 'X';
 const PlayerTurn = function () {
     return `Player ${currentPlayer} turn`;
 }
+let gameOver = false;
 
 const gameContainer = document.querySelector('.game');
 //console.log(gameContainer);
@@ -15,106 +16,114 @@ const displayMessage = document.querySelector('.game-play');
 //Player 1 will choose the first box to place their X. Also cannot choose the same box twice.
 let resetButton = document.querySelector('.reset');
 let gameStatus = displayMessage.innerHTML = 'Ready to Play!';
-
+//console.log(gameStatus);
 let playerWins = function () {
-    //console.log(playerTurn);
-    displayMessage.innerHTML = `${currentPlayer} wins!`;
+    //console.log(playerWins);
+    gameOver = true
+    if (currentPlayer === 'X') {
+        return "Player O Wins!"
+    } else {
+        return "Player X Wins"
+    }
+
 }
 
-
-
 gameContainer.addEventListener('click', (event) => {
-    //console.log('Button Group Container Clicked');
-    //console.log(event.target.getAttribute('id'));
-    if (event.target.innerText === '') {//checks for empty box
-        
-        //console.log('blah');
-        
-        event.target.innerText = currentPlayer;
-        if (currentPlayer === 'X') {
-            currentPlayer = 'O'
-            //console.log(event.target);
-            displayMessage.innerHTML = PlayerTurn();
-            //this will check to see if the player's text is X if it is it will change it to O
-        } else {
-            currentPlayer = 'X'
-            //console.log(currentPlayer);
-            displayMessage.innerHTML = PlayerTurn();
-            //this will prepare for the next player turn
+    if (gameOver !== true) {
+        //console.log('Button Group Container Clicked');
+        //console.log(event.target.getAttribute('id'));
+        if (event.target.innerText === '') {//checks for empty box
+
+            //console.log('blah');
+            event.target.innerText = currentPlayer;
+            if (currentPlayer === 'X') {
+                //console.log('currentPlayerX');
+                //checks for full box 
+                currentPlayer = 'O'
+                //console.log(event.target);
+                //console.log('currentPlayerO');
+                displayMessage.innerHTML = PlayerTurn();
+                //this will check to see if the player's text is X if it is it will change it to O
+            } else {
+                currentPlayer = 'X'
+                //console.log('currentPlayerX');
+                displayMessage.innerHTML = PlayerTurn();
+                //this will prepare for the next player turn
+            }
+            //console.log('current player');
+
+            gamePlay();
+            //i need to turn off clicks if a winner occurs
+
         }
-
-        //checks for full box 
-        //i need to turn off click
-        gamePlay();
-
-    
     }
+
 })
-//console.log('current player');
+
 function gamePlay() {
     if (document.getElementById('b0').innerText === 'X' && document.getElementById('b1').innerText === 'X' && document.getElementById('b2').innerText === 'X') {
-        displayMessage.innerHTML = `You Win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win'); 
 
     }
     else if (document.getElementById('b3').innerText === 'X' && document.getElementById('b4').innerText === 'X' && document.getElementById('b5').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');          
     }
     else if (document.getElementById('b6').innerText === 'X' && document.getElementById('b7').innerText === 'X' && document.getElementById('b8').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b0').innerText === 'O' && document.getElementById('b1').innerText === 'O' && document.getElementById('b2').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b3').innerText === 'O' && document.getElementById('b4').innerText === 'O' && document.getElementById('b5').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b6').innerText === 'O' && document.getElementById('b7').innerText === 'O' && document.getElementById('b8').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');         
     }
     else if (document.getElementById('b0').innerText === 'X' && document.getElementById('b4').innerText === 'X' && document.getElementById('b8').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');     
     }
     else if (document.getElementById('b2').innerText === 'X' && document.getElementById('b4').innerText === 'X' && document.getElementById('b6').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b0').innerText === 'X' && document.getElementById('b3').innerText === 'X' && document.getElementById('b6').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');         
     }
     else if (document.getElementById('b0').innerText === 'O' && document.getElementById('b4').innerText === 'O' && document.getElementById('b8').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');       
     }
     else if (document.getElementById('b2').innerText === 'O' && document.getElementById('b4').innerText === 'O' && document.getElementById('b6').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b0').innerText === 'O' && document.getElementById('b3').innerText === 'O' && document.getElementById('b6').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');     
     }
     else if (document.getElementById('b1').innerText === 'X' && document.getElementById('b4').innerText === 'X' && document.getElementById('b7').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');       
     }
     else if (document.getElementById('b2').innerText === 'X' && document.getElementById('b5').innerText === 'X' && document.getElementById('b8').innerText === 'X') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');         
     }
     else if (document.getElementById('b1').innerText === 'O' && document.getElementById('b4').innerText === 'O' && document.getElementById('b7').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');        
     }
     else if (document.getElementById('b2').innerText === 'O' && document.getElementById('b5').innerText === 'O' && document.getElementById('b8').innerText === 'O') {
-        displayMessage.innerHTML = `You win!`;
+        displayMessage.innerHTML = playerWins();
         //console.log(gameStatus = 'Win');       
     }
     else if (document.getElementById('b0').innerText !== '' && document.getElementById('b1').innerText !== '' && document.getElementById('b2').innerText !== '' && document.getElementById('b3').innerText !== '' && document.getElementById('b4').innerText !== '' && document.getElementById('b5').innerText !== '' && document.getElementById('b6').innerText !== '' && document.getElementById('b7').innerText !== '' && document.getElementById('b8').innerText !== '') {
@@ -125,19 +134,16 @@ function gamePlay() {
 
 
 }
-//console.log(gameStatus);
-
 
 //Is a draw when there is no more chances to win or the board is full? Is that the same thing?
 //when the board is full AND no body won
+//done gameOver function
 
 
 //Tell the player if they won/lost or tell the players they came to a draw. Ask if the players would like to play again.
-
+//done gamePlay Function
 
 //Game should reset after a Win/Lose/Draw result if the players choose Play Again.
-
-
 function restartGame() {
     //gameActive = true;
     currentPlayer = "X";
@@ -150,14 +156,6 @@ resetButton.addEventListener('click', (event) => {
     event.preventDefault();
     displayMessage.innerHTML = `Let's play again?`
     // console.log(event.button);
+    console.log('Clear');
     restartGame()
 })
-
-
-
-//Players should NOT be able to continue playing once there is a win/lose/draw.
-// const gameOver = document.
-// gameContainer.removeEventListener('click', (event))
-
-//Page should not refresh in order to play again.
-//done.
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Project 1 - Tic Tac Toe
+
+## About the Project: 
+
+Looking for a quick, simple game a 5 year old can play? Look no further than this minimally rendered game of Tic Tac Toe!
+
+Built in HTML, CSS and driven by Javascript you can easily participate in **one or one hundred rounds of fun**. 
+
+Accessible via mobile or desktop, this game will keep those kids busy for hours in the car. 
+
+———
+
+## Wireframing 
+
+I quickly sketched out what I thought the game could look like on a note card. At the beginning the user would choose their “game piece” to start the game. And the player’s wins/losses would be kept on a scoreboard. I realized later how ambitious that would be. 
+
+[My Wireframe] (https://prudentialacademy.slack.com/files/U01ACG87LQ3/F01CA7ABP9C/img_20201013_093410.jpg)
+
+———
+
+## Development
+
+I kicked off the work by pretty quickly building out the HTML page with a header and body and within the body, an H1 and div for the board. I made a smaller board, that actually fits well in a mobile format as well, so not much was needed for additional styling. 
+
+Understanding players will have to click on the boxes to insert their piece (an X or an O), I referenced back to the event propagation practice we had completed. I knew that we had developed each box to register a click and track the exact box that was clicked to the console log. Next was figuring out how to put the X or O into the box that was clicked. So I referenced our quote machine lab, where we inserted the quote we created into the list of quotes. Utilizing innerText, I was able to insert the game piece during the click event of a box on the board, alternating between X and O. On the board I also included the hover over effect so users would see which box they were going to select.
+
+In order to clear the screen, I used an event Listener connected to a button to clear the box's innerText when clicked so the players can play again. 
+
+Also I included a text H2 tag to show messages for play - who's turn, if the players win or draw and encouraging them to play again.
+
+———
+
+## Unsolved issues for Future Iterations
+
+— Would like to have the button text change - start with Play, change to Play Again when one person wins, b/c you don’t really need a Play button to start the play, you just click on the board. 
+
+———
+
+## Solving for Winner
+
+This was not as simple as I thought it was going to be, I knew of 9 possible winning combinations for the game. So to build out those combinations as represented in the boxes, I needed to call out each box's innerText as equalling the other for that combo, in "if...else" statements to reflect wins. But this left the issue of draws, when no one wins, and therefore needed to add an else statement identifying combos that were not equal and making that the draw solution.
+
+———
+
+## Favorite Functions
+
+The displayMessage function was the one I had the most fun with. Trying to make all the items say something different depending on what the player is doing at the time.
+
+———
+
+[Hosted Link] (https://kmac783.github.io/MyProject-TicTacToe/)
